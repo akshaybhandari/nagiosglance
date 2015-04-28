170af4e8e73e9185e1c9728e60fcd942af8c3bff
akshaybhandari <akshayb.2693@gmail.com>
2015-04-28 20:34:08 +0530

    Modified module for glance only checks.

--- manifests/setup/files.pp
+++ manifests/setup/files.pp
@@ -1,18 +1,2 @@
 class nagiosglance::setup::files {
-  file { "check_memory.sh_${hostname}":
-    ensure => present,
-    path   => '/usr/lib/nagios/plugins/check_memory.sh',
-    source => 'puppet:///modules/nagiosglance/check_memory.sh',
-    owner  => 'root',
-    group  => 'root',
-    mode   => '0755',
-  }
-  file { "check_service.sh_${hostname}":
-    ensure => present,
-    path   => '/usr/lib/nagios/plugins/check_service.sh',
-    source => 'puppet:///modules/nagiosglance/check_service.sh',
-    owner  => 'root',
-    group  => 'root',
-    mode   => '0755',
-  }
 }

8124973129498b87cb4d7c1975b7f973f3224af1
akshaybhandari <akshayb.2693@gmail.com>
2015-05-15 12:00:59 +0530

    Added fixes for RedHat family in 'setup::commands'

--- manifests/setup/commands.pp
+++ manifests/setup/commands.pp
@@ -2,13 +2,25 @@ class nagiosglance::setup::commands (
   $service_list = ['openvswitch-switch', 'nova-compute', 'libvirt-bin'],
   $os_type = linux,
 ) {
+  case $::osfamily {
+    'redhat': {
+      $plugin_dir = '/usr/lib64/nagios/plugins'
+      $nrpe_cfgdir = '/etc/nrpe.d'
+    }
+    'debian': {
+      $plugin_dir = '/usr/lib/nagios/plugins'
+      $nrpe_cfgdir = '/etc/nagios/nrpe.d'
+    }
+    default: { fail("No such osfamily: ${::osfamily} yet defined") }
+  }
+
   #OpenStack related checks
   each($service_list) |$service| {
     file_line { "check_service_${service}_${hostname}":
       ensure  => present,
-      path    => '/etc/nagios/nrpe.d/nrpe_command.cfg',
-      line    => "command[check_service_${service}]=/usr/lib/nagios/plugins/check_service.sh -o ${os_type} -s ${service}",
-      require => File['/etc/nagios/nrpe.d/nrpe_command.cfg'],
+      path    => "${nrpe_cfgdir}/nrpe_command.cfg",
+      line    => "command[check_service_${service}]=${plugin_dir}/check_service.sh -o ${os_type} -s ${service}",
+      require => File["${nrpe_cfgdir}/nrpe_command.cfg"],
       notify  => Service['nagios-nrpe-server'],
     }
   }
